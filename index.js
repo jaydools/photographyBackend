50d55134e1eced79ee57765e740dd03523fa5035
jaydools <134211868+jaydools@users.noreply.github.com>
2024-01-29 21:01:30 -0500

    develop branch started

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+// app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(cors());
+app.use(express.json());
+app.use("/api/stores", storeRoute);
+
+const port = process.env.PORT || 8081;
+app.listen(port, () => console.log(`Listening on port ${port}!`));
