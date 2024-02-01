fbf75d134152658d9a2d106b046d1f00dcd71153
jaydools <134211868+jaydools@users.noreply.github.com>
2024-02-01 17:22:00 -0500

    backend sucessfully making request

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
+const Buffer = require("buffer/").Buffer;
 
 const app = express();
-// app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(cors());
 app.use(express.json());
+
+app.get("/images", async (req, res) => {
+    try {
+        const response = await fetch(
+            `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`,
+            {
+                headers: {
+                    Authorization: `Basic ${Buffer.from(
+                        process.env.CLOUDINARY_API_KEY + ":" + process.env.CLOUDINARY_API_SECRET
+                    ).toString("base64")}`,
+                },
+            }
+        );
+
+        const data = await response.json();
+
+        res.json(data);
+    } catch (err) {
+        console.error("unknown error... initiate self destruct", err);
+    }
+});
 
 const port = process.env.PORT || 8081;
 app.listen(port, () => console.log(`Listening on port ${port}!`));
